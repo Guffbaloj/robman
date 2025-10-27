58112c66b74eae3e346ffd2047e4261da882e4be
Simon Andersson <simon.w.andersson06@gmail.com>
2025-10-27 12:43:48 +0100

    initial commit

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 import pygame
+WIDTH = 820
+HEIGHT = 640
+class Main:
+    def __init__(self):
+        self.window = pygame.display.set_mode((WIDTH,HEIGHT))
+    
+    def run(self):
+        while True:
+            self.window.fill((255,255,255))
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+            
+            pygame.display.update()
+
+if __name__ == "__main__":
+    Main().run()
